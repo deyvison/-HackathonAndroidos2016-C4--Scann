6454cb44168ec775066fe9d0cea6edd5b9120dff
deyvison <deyvison.melo@dce.ufpb.br>
2016-06-29 10:47:05 -0300

    Criando o projeto

---
+++ app/src/test/java/br/com/ufpb/c4/ayty/hackathonandroidos2016_c4_scann/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package br.com.ufpb.c4.ayty.hackathonandroidos2016_c4_scann;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
